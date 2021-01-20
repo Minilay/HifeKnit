a0c6858a27db824afc406803b70185f461d4f5be
Minilay <minilie88@gmail.com>
2021-01-20 16:21:50 +0600

    Short

---
+++ Assets/_Scripts/Knife.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Knife : MonoBehaviour
+{
+    private Rigidbody2D rigid;
+    private void Start()
+    {
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.CompareTag("Knife") && transform.CompareTag("Player"))
+        {
+            GetComponent<BoxCollider2D>().enabled = false;
+            rigid = GetComponent<Rigidbody2D>();
+            print("GameOver");
+
+            rigid.constraints = RigidbodyConstraints2D.None;
+            rigid.velocity = Vector3.zero;
+            rigid.AddTorque(Random.Range(400, 900));
+            rigid.AddForce(Vector3.right * Random.Range(-10,10));
+            rigid.gravityScale = 1;
+            StartCoroutine(Controller.S.reload());
+        }
+    }
+}
---
+++ Assets/_Scripts/Log.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Log : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float rotateSpeed;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(!Controller.S.dying)
+        {
+            collision.gameObject.transform.SetParent(transform);
+            collision.transform.tag = "Knife";
+            collision.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+            collision.gameObject.GetComponent<Knife>().enabled = false;
+        }
+
+        Controller.S.Spawn();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, rotateSpeed * Time.deltaTime);
+    }
+}
